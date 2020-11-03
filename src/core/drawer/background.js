4d1cbe943ac194b82784e36698faca76b6f1aefa
srdn <srdn2417@gmail.com>
2020-11-03 19:17:43 +0200

    background

--- src/core/drawer/background.js
+++ src/core/drawer/background.js
@@ -1,8 +1,19 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   const node = document.querySelector('#game');
-  const ctx = node.getContext('2d');
 
-  ctx.fillStyle = 'green';
-  ctx.fillRect(10, 10, 100, 100);
+  Object.assign(node.style, {
+    width: '640px',
+    height: '480px',
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%, -50%)',
+    border: '3px solid red',
+    background: 'black',
+  });
+
+  // const ctx = node.getContext('2d');
+  // ctx.fillStyle = 'black';
+  // ctx.fillRect(0, 0, 640, 480);
 });
 
